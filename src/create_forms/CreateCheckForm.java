4011fa76099f136d506c9cabc7033d486410ba1b
Maryna Podina <m.rodina1210@gmail.com>
2023-04-26 00:43:11 +0200

    fixed reloading of creating a receipt

--- src/create_forms/CreateCheckForm.java
+++ src/create_forms/CreateCheckForm.java
@@ -1,6 +1,7 @@
 package create_forms;
 
 import bd_connection.Check;
+import bd_connection.Customer_Card;
 import bd_connection.Store_Product;
 import entity.*;
 import info_menu_cashier.ReceiptViewCashier;
@@ -23,6 +24,8 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.io.IOException;
 import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Timestamp;
 import java.time.LocalDate;
@@ -35,27 +38,41 @@ import static bd_connection.Store_Product.getAllProductsInStoreSaleSorted;
 import static bd_connection.Store_Product.getAllProductsInStoreSorted;
 import static entity.Employee.Role.CASHIER;
 
-public class CreateCheckForm extends JFrame {
+public class CreateCheckForm{
 
     private JPanel checkPanel;
     private JFrame frame;
     private Employee employee;
     private CustomerCard card;
     private HashMap<String,SoldProduct> added;
+    private Connection connection;
 
-    public CreateCheckForm(JFrame frame, Employee employee, CustomerCard card) {
+    public CreateCheckForm(JFrame frame, Employee employee) {
         this.frame = frame;
         this.employee = employee;
-        this.card = card;
         added = new HashMap<>();
-        this.setBounds(800,400,500,350);
+        frame.setBounds(800,400,500,350);
         init();
     }
 
     private void init() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3308/zlagoda",
+                    "root",
+                    ""
+            );
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+        Store_Product.setConnection(connection);
+        Check.setConnection(connection);
+        bd_connection.Category.setConnection(connection);
+        Customer_Card.setConnection(connection);
         checkPanel = new JPanel(new GridBagLayout());
         GridBagConstraints c = new GridBagConstraints();
-        add(checkPanel);
+        frame.add(checkPanel);
 
         JButton backButton = new JButton("Back");
         backButton.addMouseListener(new MouseAdapter() {
@@ -73,10 +90,24 @@ public class CreateCheckForm extends JFrame {
         c.gridy = 0;
         checkPanel.add(backButton,c);
 
+        JLabel filler1 = new JLabel("");
+        c.gridx = 1;
+        c.gridy = 0;
+        checkPanel.add(filler1,c);
+        JLabel filler2 = new JLabel("");
+        c.gridx = 2;
+        c.gridy = 0;
+        checkPanel.add(filler2,c);
+        JLabel filler3 = new JLabel("");
+        c.gridx = 3;
+        c.gridy = 0;
+        c.ipadx = frame.getWidth()-270;
+        checkPanel.add(filler3,c);
+
         JLabel addedLabel = new JLabel("Added products:");
         c.gridx = 0;
         c.gridy = 1;
-        c.ipadx = this.getWidth()-200;
+        c.ipadx = 0;
         checkPanel.add(addedLabel,c);
 
         ArrayList<String[]> values = new ArrayList<String[]>();
@@ -93,7 +124,7 @@ public class CreateCheckForm extends JFrame {
         scrollPane.setVerticalScrollBarPolicy(
                 JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
         scrollPane.setVisible(true);
-        c.gridwidth = 2;
+        c.gridwidth = 4;
         c.gridx = 0;
         c.gridy = 2;
         c.ipady = 200;
@@ -117,21 +148,16 @@ public class CreateCheckForm extends JFrame {
         c.fill = GridBagConstraints.NONE;
         checkPanel.add(addButton,c);
 
-        JButton createButton = new JButton("Create");
-        createButton.addMouseListener(new MouseAdapter() {
+        JButton customerButton = new JButton("Choose customer");
+        customerButton.addMouseListener(new MouseAdapter() {
             @Override
             public void mousePressed(MouseEvent e) {
                 super.mousePressed(e);
+                frame.setEnabled(false);
                 try {
-                    createReceipt();
-                    JOptionPane.showMessageDialog(new JFrame(), "New receipt created", "Success",
-                            JOptionPane.PLAIN_MESSAGE);
-                    frame.getContentPane().removeAll();
-                    ReceiptViewCashier.display(frame, employee);
-                    // Repaint the frame
-                    frame.revalidate();
-                    frame.repaint();
+                    chooseCustomer();
                 }catch (SQLException ex){
+                    ex.printStackTrace();
                     JOptionPane.showMessageDialog(new JFrame(), "Error in database occurred", "Error",
                             JOptionPane.ERROR_MESSAGE);
                 }
@@ -139,23 +165,134 @@ public class CreateCheckForm extends JFrame {
         });
         c.gridx = 1;
         c.gridy = 3;
+        checkPanel.add(customerButton,c);
+
+        JButton createButton = new JButton("Create");
+        createButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                super.mousePressed(e);
+                try {
+                    boolean success = createReceipt();
+                    if(success) {
+                        JOptionPane.showMessageDialog(new JFrame(), "New receipt created", "Success",
+                                JOptionPane.PLAIN_MESSAGE);
+                        frame.getContentPane().removeAll();
+                        ReceiptViewCashier.display(frame, employee);
+                        // Repaint the frame
+                        frame.revalidate();
+                        frame.repaint();
+                    }
+                }catch (SQLException ex){
+                    ex.printStackTrace();
+                    JOptionPane.showMessageDialog(new JFrame(), "Error in database occurred", "Error",
+                            JOptionPane.ERROR_MESSAGE);
+                }
+            }
+        });
+        c.gridx = 3;
+        c.gridy = 3;
         checkPanel.add(createButton,c);
     }
 
-    private void createReceipt() throws SQLException {
+    private void chooseCustomer() throws SQLException {
+        JFrame tempFrame = new JFrame();
+        List<CustomerCard> customersList = Customer_Card.getAllCustomersSorted(true);
+        JToolBar buttonPanel = new JToolBar();
+
+        JButton cancelButton = new JButton("Cancel");
+        buttonPanel.add(cancelButton);
+
+        cancelButton.addActionListener(s -> {
+            tempFrame.setVisible(false);
+        });
+
+        JPanel tablePanel = new JPanel(new BorderLayout());
+
+        JTable table = new JTable();
+
+        DefaultTableModel model = new DefaultTableModel(new Object[]{"Number", "Name", "Surname", "Patronymic", "City", "Street", "ZipCode", "Phone number", "Percent"}, 0) {
+            @Override
+            public boolean isCellEditable(int row, int column) {
+                return false; // make the cells non-editable
+            }
+        };
+        table.setModel(model);
+
+        for (CustomerCard cc : customersList) {
+            model.addRow(new Object[]{cc.getNumber(), cc.getName(), cc.getSurname(), cc.getPatronymic(), cc.getCity(), cc.getStreet(), cc.getZipCode(), cc.getPhoneNumber(), cc.getPercent()});
+        }
+
+        JScrollPane scrollPane = new JScrollPane(table);
+
+        tablePanel.add(scrollPane, BorderLayout.CENTER);
+
+
+        JToolBar searchTools = new JToolBar();
+        JButton searchUPC = new JButton("Search");
+        searchTools.add(searchUPC);
+
+        tempFrame.add(buttonPanel, BorderLayout.PAGE_START);
+
+        tempFrame.add(tablePanel, BorderLayout.CENTER);
+
+
+
+        tempFrame.setSize(500, 500);
+        tempFrame.setVisible(true);
+        AtomicBoolean sortAlph = new AtomicBoolean(true);
+
+        searchUPC.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                tempFrame.getContentPane().removeAll();
+                StoreProductCashier.display(tempFrame, employee);
+                // Repaint the frame
+                tempFrame.revalidate();
+                tempFrame.repaint();
+            }
+        });
+
+        table.getSelectionModel().addListSelectionListener(e -> {
+            if (!e.getValueIsAdjusting()) {
+                int row = table.getSelectedRow();
+                if (row >= 0) {
+                    tempFrame.setEnabled(false);
+                    CustomerCard customer = customersList.get(row);
+                    card = customer;
+                    tempFrame.setVisible(false);
+                    JOptionPane.showMessageDialog(new JFrame(), "Customer was chosen", "Success",
+                            JOptionPane.PLAIN_MESSAGE);
+                    frame.setEnabled(true);
+                }
+            }
+        });
+    }
+
+    private boolean createReceipt() throws SQLException {
         BigDecimal sum = BigDecimal.valueOf(0);
         for (SoldProduct product: added.values()) {
             sum = sum.add(product.getPrice().multiply(BigDecimal.valueOf(product.getAmount())));
         }
         BigDecimal vat = sum.multiply(BigDecimal.valueOf(0.2));
         sum = sum.add(vat);
-        Receipt receipt = new Receipt(generateNumber(),employee,card, Timestamp.valueOf(LocalDateTime.now()),sum,vat, (List<SoldProduct>) added.values());
-        Check.AddNewReceipt(receipt);
-        for (SoldProduct product: added.values()) {
-            ProductInStore inStore = Store_Product.findProductInStoreById(product.getUPC());
-            Store_Product.updateProductInStoreById(new ProductInStore(inStore.getUPC(),inStore.getPromotionalUPC(),inStore.getProduct(),
-                    inStore.getPrice(),inStore.getAmount()-product.getAmount(),inStore.isPromotional()));
+        ArrayList<SoldProduct> sold = new ArrayList<>();
+        for (String key: added.keySet()) {
+            sold.add(added.get(key));
         }
+        if(sold.isEmpty()){
+            JOptionPane.showMessageDialog(new JFrame(), "No products were added", "Error",
+                    JOptionPane.ERROR_MESSAGE);
+            return false;
+        }
+        Receipt receipt = new Receipt(generateNumber(), employee, card, Timestamp.valueOf(LocalDateTime.now()), sum, vat, sold);
+        Check.AddNewReceipt(receipt);
+        for (SoldProduct product : added.values()) {
+            ProductInStore inStore = Store_Product.findProductInStoreById(product.getUPC());
+            Store_Product.updateProductInStoreById(new ProductInStore(inStore.getUPC(), inStore.getPromotionalUPC(), inStore.getProduct(),
+                    inStore.getPrice(), inStore.getAmount() - product.getAmount(), inStore.isPromotional()));
+        }
+        return true;
     }
 
     public String generateNumber() {
@@ -307,6 +444,8 @@ public class CreateCheckForm extends JFrame {
                     int amount = askAmount(product.getAmount()-amountThere);
                     added.put(product.getUPC(),new SoldProduct(product.getUPC(),product.getProduct().getName(),amount+amountThere,product.getPrice()));
                     tempFrame.setVisible(false);
+                    frame.getContentPane().removeAll();
+                    init();
                     frame.revalidate();
                     frame.repaint();
                     frame.setEnabled(true);
@@ -334,8 +473,9 @@ public class CreateCheckForm extends JFrame {
     }
 
     public static void main(String[] args) throws IOException, UnsupportedAudioFileException {
-        CreateCheckForm a = new CreateCheckForm(new JFrame(),new Employee("","","","","",CASHIER,BigDecimal.valueOf(0),
-                null,null,"","","",""),null);
-        a.setVisible(true);
+        JFrame frame = new JFrame();
+        CreateCheckForm a = new CreateCheckForm(frame,new Employee("","","","","",CASHIER,BigDecimal.valueOf(0),
+                null,null,"","","",""));
+        frame.setVisible(true);
     }
 }
